01ad9455aadd8453878fa4b11a2a3df8043232b0
Emma Sharon <emmasharon19@gmail.com>
2019-04-28 15:33:35 +0300

    declair variables in the js file

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var result() {
+  var a = parseFloat(document.getElementById('Value 1').value);
+  var b = parseFloat(document.getElementById('Value 2').value);
+  var c = parseFloat(document.getElementById('Value 3').value);
+  var answer = document.getElementById('answer')
+}
