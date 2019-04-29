e409f0fb29e47b1c6c696f6c442a5a3199e77aec
Emma Sharon <emmasharon19@gmail.com>
2019-04-29 07:40:05 +0300

    fix errors on javascript

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
-var result() {
-  var a = parseFloat(document.getElementById('Value 1').value);
-  var b = parseFloat(document.getElementById('Value 2').value);
-  var c = parseFloat(document.getElementById('Value 3').value);
+function result() {
+  var a = parseFloat(document.getElementById('Value1').value);
+  var b = parseFloat(document.getElementById('Value2').value);
+  var c = parseFloat(document.getElementById('Value3').value);
   var answer = document.getElementById('answer');
 
 if (a+b>c && b+c>a && c+a>b){
 
-  if (a!===b && b!==c && c!===a){
+  if (a!==b && b!==c && c!==a){
     answer.textContent="SCALENE";
   }
 
@@ -14,7 +14,7 @@ if (a+b>c && b+c>a && c+a>b){
     answer.textContent="EQUILATERAL";
   }
 
-  else (a===b || b===c || c===a){
+  else if (a===b || b===c || c===a){
     answer.textContent="ISOSCELES";
   }
 }
@@ -22,9 +22,9 @@ else {
   answer.textContent="That is not a triangle";
 }
 }
-function reset(){
-  var Value 1=parseInt(document.getElementById('Value 1').value);
-  var Value 2=parseInt(document.getElementById('Value 2').value);
-  var Value 3=parseInt(document.getElementById('Value 3').value);
-  var answer=document.getElementById('answer');
-}
+// function reset(){
+//   // var Value 1=parseInt(document.getElementById('Value 1').value);
+//   var Value 2=parseInt(document.getElementById('Value 2').value);
+//   var Value 3=parseInt(document.getElementById('Value 3').value);
+//   var answer=document.getElementById('answer');
+// }
